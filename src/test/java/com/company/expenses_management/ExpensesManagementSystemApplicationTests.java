70dc115af1d56b759607eb93a099f05e675b17ac
AA007RS <selenicarubin@gmail.com>
2025-01-15 23:26:11 +0100

    Initial commit

---
+++ src/test/java/com/company/expenses_management/ExpensesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.expenses_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
